6b2ac95babe3c05412e6627cfadcf80d13a1f3ff
Frederico Jordan <fredericojordan@gmail.com>
2018-01-24 00:14:18 -0200

    Replacing tabs for spaces

--- outputs_01-23_Todos.py
+++ outputs_01-23_Todos.py
@@ -31,136 +31,136 @@ dados['maior26'] = []
 dados['timesteps'] = []
 
 def take_idf(files):
-	idf_files = []
+    idf_files = []
 
-	for file in files:
-		if str(file).endswith(".idf"):
-			idf_files.append(file)
+    for file in files:
+        if str(file).endswith(".idf"):
+            idf_files.append(file)
 
-	return idf_files
+    return idf_files
 
 def horasConforto(file,zona):
-	
-	menor16 = 0
-	entre16e18 = 0
-	entre18e23 = 0
-	entre23e26 = 0
-	maior26 = 0
-	timesteps = 0
+    
+    menor16 = 0
+    entre16e18 = 0
+    entre18e23 = 0
+    entre23e26 = 0
+    maior26 = 0
+    timesteps = 0
 
-	
-	if zona == 'SALA':
+    
+    if zona == 'SALA':
 
-		try:
-			timesteps = (file['SALA1:People Occupant Count [](TimeStep)'] > 0).value_counts()[True]
-		except:
-			timesteps = 0
+        try:
+            timesteps = (file['SALA1:People Occupant Count [](TimeStep)'] > 0).value_counts()[True]
+        except:
+            timesteps = 0
 
-		try:
-			menor16 = ((file[zona+':Zone Operative Temperature [C](TimeStep)'] < 16) & (file['SALA:People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
-		except:
-			menor16 = 0
+        try:
+            menor16 = ((file[zona+':Zone Operative Temperature [C](TimeStep)'] < 16) & (file['SALA:People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
+        except:
+            menor16 = 0
 
-		try:	
-			entre16e18 = ((file[zona+':Zone Operative Temperature [C](TimeStep)'] >= 16) & (file[zona+':Zone Operative Temperature [C](TimeStep)'] < 18) & (file['SALA1:People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
-		except:
-			entre16e18 = 0
-		
-		try:
-			entre18e23 = ((file[zona+':Zone Operative Temperature [C](TimeStep)'] >= 18) & (file[zona+':Zone Operative Temperature [C](TimeStep)'] < 23) & (file['SALA1:People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
-		except:
-			entre18e23 = 0
+        try:    
+            entre16e18 = ((file[zona+':Zone Operative Temperature [C](TimeStep)'] >= 16) & (file[zona+':Zone Operative Temperature [C](TimeStep)'] < 18) & (file['SALA1:People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
+        except:
+            entre16e18 = 0
+        
+        try:
+            entre18e23 = ((file[zona+':Zone Operative Temperature [C](TimeStep)'] >= 18) & (file[zona+':Zone Operative Temperature [C](TimeStep)'] < 23) & (file['SALA1:People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
+        except:
+            entre18e23 = 0
 
-		try:
-			entre23e26 = ((file[zona+':Zone Operative Temperature [C](TimeStep)'] >= 23) & (file[zona+':Zone Operative Temperature [C](TimeStep)'] < 26) & (file['SALA1:People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
-		except:
-			entre23e26 = 0	
-		
-		try:
-			maior26 = ((file[zona+':Zone Operative Temperature [C](TimeStep)'] >= 26) & (file['SALA1:People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
-		except:
-			maior26 = 0
-	
-	else:
+        try:
+            entre23e26 = ((file[zona+':Zone Operative Temperature [C](TimeStep)'] >= 23) & (file[zona+':Zone Operative Temperature [C](TimeStep)'] < 26) & (file['SALA1:People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
+        except:
+            entre23e26 = 0    
+        
+        try:
+            maior26 = ((file[zona+':Zone Operative Temperature [C](TimeStep)'] >= 26) & (file['SALA1:People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
+        except:
+            maior26 = 0
+    
+    else:
 
-		try:
-			timesteps = (file['DORMITORIO'+zona+':People Occupant Count [](TimeStep)'] > 0).value_counts()[True]
-		except:
-			timesteps = 0
+        try:
+            timesteps = (file['DORMITORIO'+zona+':People Occupant Count [](TimeStep)'] > 0).value_counts()[True]
+        except:
+            timesteps = 0
 
-		try:
-			menor16 = ((file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] < 16) & (file['DORMITORIO'+zona+':People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
-		except:
-			menor16 = 0
-		
-		try:
-			entre16e18 = ((file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] >= 16) & (file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] < 18) & (file['DORMITORIO'+zona+':People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
-		except:
-			entre16e18 = 0
+        try:
+            menor16 = ((file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] < 16) & (file['DORMITORIO'+zona+':People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
+        except:
+            menor16 = 0
+        
+        try:
+            entre16e18 = ((file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] >= 16) & (file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] < 18) & (file['DORMITORIO'+zona+':People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
+        except:
+            entre16e18 = 0
 
-		try:
-			entre18e23 = ((file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] >= 18) & (file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] < 23) & (file['DORMITORIO'+zona+':People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
-		except:
-			entre18e23 = 0
+        try:
+            entre18e23 = ((file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] >= 18) & (file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] < 23) & (file['DORMITORIO'+zona+':People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
+        except:
+            entre18e23 = 0
 
-		try:
-			entre23e26 = ((file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] >= 23) & (file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] < 26) & (file['DORMITORIO'+zona+':People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
-		except:
-			entre23e26 = 0
+        try:
+            entre23e26 = ((file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] >= 23) & (file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] < 26) & (file['DORMITORIO'+zona+':People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
+        except:
+            entre23e26 = 0
 
-		try:	
-			maior26 = ((file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] >= 26) & (file['DORMITORIO'+zona+':People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
-		except:
-			maior26 = 0
-	
-	return(menor16,entre16e18,entre18e23,entre23e26,maior26,timesteps)
+        try:    
+            maior26 = ((file['DORM'+zona+':Zone Operative Temperature [C](TimeStep)'] >= 26) & (file['DORMITORIO'+zona+':People Occupant Count [](TimeStep)'] > 0)).value_counts()[True]
+        except:
+            maior26 = 0
+    
+    return(menor16,entre16e18,entre18e23,entre23e26,maior26,timesteps)
 
 
 for pasta in pastas:
 
-	os.chdir(dir+'\\'+pasta)
-	files = os.listdir(os.getcwd())
-	files.sort()
-	idf_files = take_idf(files)
+    os.chdir(dir+'\\'+pasta)
+    files = os.listdir(os.getcwd())
+    files.sort()
+    idf_files = take_idf(files)
 
-	for file in idf_files:
-		
-		print(file)
-		
-		file = file[:-4]
-		openfile = pd.read_csv(file+'.csv')
-		
-		for zona in zonas:
-			
-			if zona == 'SALA':
-				
-				heatingkey = (zona + ' IDEAL LOADS AIR SYSTEM:Zone Ideal Loads Supply Air Total Heating Energy [J](TimeStep)')
-			else:
-				heatingkey = ('DORM'+zona + ' IDEAL LOADS AIR SYSTEM:Zone Ideal Loads Supply Air Total Heating Energy [J](TimeStep)')
-			heatingIdealLoads = sum(openfile[heatingkey])
-				
-			if zona == 'SALA':
-				
-				coolingkey = (zona + ' IDEAL LOADS AIR SYSTEM:Zone Ideal Loads Supply Air Total Cooling Energy [J](TimeStep)')
-			else:
-				coolingkey = ('DORM'+zona + ' IDEAL LOADS AIR SYSTEM:Zone Ideal Loads Supply Air Total Cooling Energy [J](TimeStep)')
-			
-			coolingIdealLoads = sum(openfile[coolingkey])
-				
-			horas = horasConforto(openfile,zona) # fracao de horas
-				
-			dados['pasta'].append(pasta)
-			dados['arquivo'].append(file)
-			dados['zona'].append(zona)
-			dados['heating'].append(heatingIdealLoads)
-			dados['cooling'].append(coolingIdealLoads)
-			dados['menor16'].append(horas[0])
-			dados['entre16e18'].append(horas[1])
-			dados['entre18e23'].append(horas[2])
-			dados['entre23e26'].append(horas[3])
-			dados['maior26'].append(horas[4])
-			dados['timesteps'].append(horas[5])
-		
+    for file in idf_files:
+        
+        print(file)
+        
+        file = file[:-4]
+        openfile = pd.read_csv(file+'.csv')
+        
+        for zona in zonas:
+            
+            if zona == 'SALA':
+                
+                heatingkey = (zona + ' IDEAL LOADS AIR SYSTEM:Zone Ideal Loads Supply Air Total Heating Energy [J](TimeStep)')
+            else:
+                heatingkey = ('DORM'+zona + ' IDEAL LOADS AIR SYSTEM:Zone Ideal Loads Supply Air Total Heating Energy [J](TimeStep)')
+            heatingIdealLoads = sum(openfile[heatingkey])
+                
+            if zona == 'SALA':
+                
+                coolingkey = (zona + ' IDEAL LOADS AIR SYSTEM:Zone Ideal Loads Supply Air Total Cooling Energy [J](TimeStep)')
+            else:
+                coolingkey = ('DORM'+zona + ' IDEAL LOADS AIR SYSTEM:Zone Ideal Loads Supply Air Total Cooling Energy [J](TimeStep)')
+            
+            coolingIdealLoads = sum(openfile[coolingkey])
+                
+            horas = horasConforto(openfile,zona) # fracao de horas
+                
+            dados['pasta'].append(pasta)
+            dados['arquivo'].append(file)
+            dados['zona'].append(zona)
+            dados['heating'].append(heatingIdealLoads)
+            dados['cooling'].append(coolingIdealLoads)
+            dados['menor16'].append(horas[0])
+            dados['entre16e18'].append(horas[1])
+            dados['entre18e23'].append(horas[2])
+            dados['entre23e26'].append(horas[3])
+            dados['maior26'].append(horas[4])
+            dados['timesteps'].append(horas[5])
+        
 #print(dados)
 
 os.chdir(dir)
